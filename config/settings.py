7e1d542768b3d41bcaa0b91e33120ab27444cf03
Fercriez <fernando.olivares17@inacapmail.cl>
2026-06-12 14:56:53 -0400

    Arreglar ALLOWED_HOSTS para permitir la EC2

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-z(g7f+#08!m7bzs10cfy3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
