b580f425f5ce46c8deea78f5fe00d9ba5f2aae47
Fercriez <fernando.olivares17@inacapmail.cl>
2026-07-03 11:56:15 -0400

    sadasd

--- store/models.py
+++ store/models.py
@@ -13,7 +13,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.URLField(blank=True, null=True)
-    image = models.ImageField(upload_to='productos/', blank=True, null=True)
+    image = models.ImageField(upload_to='', blank=True, null=True)
 
     def __str__(self):
         return self.name
