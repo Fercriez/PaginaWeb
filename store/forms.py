eea8c949c8f16a3b85dbf1762230d0e302c4ac78
Fercriez <fernando.olivares17@inacapmail.cl>
2026-07-03 15:24:12 -0400

    pruebas2.044

--- store/forms.py
+++ store/forms.py
@@ -6,9 +6,13 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'description', 'price', 'category', 'image']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ej. Zapatillas'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Ej. 25000'}),
-            'category': forms.Select(attrs={'class': 'form-control'}),
-            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={
+                'class': 'w-full p-2 border border-gray-300 rounded-md'
+            }),
+            'description': forms.Textarea(attrs={
+                'class': 'w-full p-2 border border-gray-300 rounded-md', 'rows': 3
+            }),
+            'price': forms.NumberInput(attrs={'class': 'w-full p-2 border border-gray-300 rounded-md'}),
+            'category': forms.Select(attrs={'class': 'w-full p-2 border border-gray-300 rounded-md'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'w-full p-2 border border-gray-300 rounded-md'}),
         }
